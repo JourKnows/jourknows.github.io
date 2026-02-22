bb036c25280b8ef2f8e67cf41d408a4624023357
arsg0etia <95715389+arsg0etia@users.noreply.github.com>
2026-02-22 19:24:45 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://jourknows.github.io/", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "Journalism for All!",
   title: "JourKnows",
