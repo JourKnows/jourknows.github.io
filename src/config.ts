6c9730de1e46e9245951b2c71f00dc253667176a
arsg0etia <95715389+arsg0etia@users.noreply.github.com>
2026-02-22 19:23:24 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@ import type { Site, SocialObjects } from "./types";
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  desc: "Journalism for All!",
+  title: "JourKnows",
   ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
+  lightAndDarkMode: false,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
